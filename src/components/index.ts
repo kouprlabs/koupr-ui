5e06b9284c0701a818a98fccdd72cfb7d4ee8830
Elias Bouassaba <ebouassaba@gmail.com>
2023-12-16 03:34:35 +0100

    feat: introduce Shell component, set version to 2.0.0

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { Drawer, DrawerItem, DrawerContext } from './drawer'
+export * from './drawer'
 export * from './icons'
-export { Spinner } from './spinner'
-export { SectionSpinner } from './section-spinner'
+export * from './spinner'
+export * from './section-spinner'
+export * from './shell'
