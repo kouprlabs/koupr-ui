3f835c562e250137edd5c4ec806d6a63c217b840
Anass Bouassaba <ebouassaba@gmail.com>
2024-10-29 17:55:14 +0100

    wip: add pagination and page-pagination components

---
+++ src/stories/components/page-pagination.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { PagePagination } from '../../components'
+import usePagePagination from '../../hooks/page-pagination'
+
+const meta: Meta<typeof PagePagination> = {
+  title: 'Components/Page Pagination',
+  component: PagePagination,
+}
+
+export default meta
+type Story = StoryObj<typeof PagePagination>
+
+export const Default: Story = {
+  args: {
+    totalElements: 100,
+    totalPages: 20,
+  },
+  render: (args) => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { page, size, steps, setPage, setSize } = usePagePagination({
+      navigate,
+      location,
+      storage: {
+        prefix: 'page-pagination',
+        namespace: 'main',
+      },
+    })
+
+    return (
+      <PagePagination
+        {...args}
+        page={page}
+        size={size}
+        steps={steps}
+        setPage={setPage}
+        setSize={setSize}
+      />
+    )
+  },
+}
