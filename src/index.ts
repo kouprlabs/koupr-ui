1555339c580c961a3dbe023eae54e8e4c757b31f
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-05 21:18:28 +0100

    fix: export common-types (#10)

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import { variables } from './variables'
 export * from './components'
 export * from './hooks'
 export * from './react-select'
+export * from './common-types'
 
 export { theme, variables }
