4ebae18c55a32f865cbf6ca15d293107dba70cdb
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-21 04:48:57 +0100

    chore: no .md extension for license file

--- src/stories/components/form.stories.tsx
+++ src/stories/components/form.stories.tsx
@@ -1,7 +1,7 @@
 // Copyright 2023 Anass Bouassaba.
 //
 // This software is licensed under the MIT License.
-// You can find a copy of the license in the LICENSE.md file
+// You can find a copy of the license in the LICENSE file
 // included in the root of this repository or at
 // https://opensource.org/licenses/MIT.
 import { IconButton, Progress, Switch } from '@chakra-ui/react'
