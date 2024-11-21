4e3430ee2f986ac9fd7bc6c979c49c31e6961fc9
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-21 06:10:57 +0100

    chore: refine license header

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,9 +1,8 @@
 // Copyright (c) 2023 Anass Bouassaba.
 //
 // This software is licensed under the MIT License.
-// You can find a copy of the license in the LICENSE file
-// included in the root of this repository or at
-// https://opensource.org/licenses/MIT.
+// See the LICENSE file in the root of this repository for details,
+// or visit <https://opensource.org/licenses/MIT>.
 
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../dist/types.d.ts" />
